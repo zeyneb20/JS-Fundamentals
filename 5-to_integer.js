89c0a5fa13a105c092d59abdd79621b845e1d802
zineb ahfid <zineb_ahfid@um5.ac.ma>
2025-05-25 19:20:51 +0100

    Add 5-to_integer.js

---
+++ 5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const arg = process.argv[2];
+const number = parseInt(arg);
+
+if (!isNaN(number)) {
+  console.log(`My number: ${number}`);
+} else {
+  console.log('Not a number');
+}
